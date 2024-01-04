ca95cfc2e2f0735a82871647240f825d3530f31f
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-04 08:14:33 -0800

    Adding 2-args.py file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+len = len(argv)
+
+if len == 1:
+    print("0 arguments")
+
+elif len == 2:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+
+else:
+    print("{} arguments:".format(len - 1))
+    for i in range(1, len):
+        print("{}: {}".format(i, argv[i]))
