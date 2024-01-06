448ba36a5caec7547c10058f8805d8ab8d219a9c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-05 23:41:33 -0800

    Fixing 0-print_list_integer.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    l = len(my_list)
-    if idx >= l or idx < 0:
+    length = len(my_list)
+    if idx >= length or idx < 0:
         return None
 
     return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    l = len(my_list)
-    if idx >= l or idx < 0:
+    length = len(my_list)
+    if idx >= length or idx < 0:
         return my_list
 
     else:
