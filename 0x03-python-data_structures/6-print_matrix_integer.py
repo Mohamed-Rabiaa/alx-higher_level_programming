25ef3d525c41c9592aa2c5ca9cd51fbdba306fa8
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-06 05:20:38 -0800

    Fixing 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end = ' ')
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end=' '
+                  if j != len(matrix[i]) - 1 else '')
 
         print()
