56d3b85a09c910098a2dd47a7400b65585b1124e
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-10 11:56:59 -0800

    Adding 2-uniq_add func & improving 1-search_replace func

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in my_list:
-        if i == search:
-            i = replace
-        new_list.append(i)
 
-    return new_list
+    return [replace if i == search else i for i in my_list]
