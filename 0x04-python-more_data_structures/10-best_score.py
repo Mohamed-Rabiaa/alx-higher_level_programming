5067198f4fd9c921c3697e2e4fae468cf95e3327
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-10 15:34:55 -0800

    Addin 10-best_score func

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_max_value(my_list=[]):
+    max_value = my_list[0]
+    for i in my_list:
+        if i > max_value:
+            max_value = i
+    return max_value
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    values_list = list(a_dictionary.values())
+    max_value = list_max_value(values_list)
+
+    for x,y in a_dictionary.items():
+        if y == max_value:
+            return x
