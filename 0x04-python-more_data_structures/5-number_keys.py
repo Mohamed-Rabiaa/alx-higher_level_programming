5742503786f201cc52ba7485e5e74851f0d712b9
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-10 13:02:27 -0800

    Fixing 5-number_keys func

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
+    if not a_dictionary:
+        return 0
     keys_list = list(a_dictionary)
     keys_counter = 0
     for i in range(len(keys_list)):
