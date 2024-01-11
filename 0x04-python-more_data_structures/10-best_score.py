8429e7c84dc4fec248e718ed4cd9430ad0a8cd9b
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-10 16:17:50 -0800

    Adding multiply_list_map func

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,12 +6,13 @@ def list_max_value(my_list=[]):
             max_value = i
     return max_value
 
+
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
     values_list = list(a_dictionary.values())
     max_value = list_max_value(values_list)
 
-    for x,y in a_dictionary.items():
+    for x, y in a_dictionary.items():
         if y == max_value:
             return x
