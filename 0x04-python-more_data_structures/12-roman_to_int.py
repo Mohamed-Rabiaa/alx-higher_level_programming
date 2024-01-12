42011d112dda56ea6a8d432b533a85a0a5c16da5
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-12 06:05:39 -0800

    Adding roman_to_int func

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    n = 0
+    str_len = len(roman_string)
+    if str_len == 0:
+        return 0
+
+    if roman_string == None or len(roman_string) < 1:
+        return 0
+
+    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                 'M': 1000}
+
+    for i in range(str_len):
+        if roman_string[i] in roman_dic:
+            if i != str_len - 1 and roman_dic[roman_string[i]] <\
+            roman_dic[roman_string[i + 1]]:
+                n -= roman_dic[roman_string[i]]
+            else:
+                n += roman_dic[roman_string[i]]
+
+        else:
+            return 0
+
+    return n
