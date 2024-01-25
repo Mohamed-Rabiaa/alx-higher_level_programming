9d24998d5d3d29ffe90caf3af164cd103f80c9c6
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-24 19:10:26 -0800

    Adding the position attribute to the Square class

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" This module defines the class 'Square' """
+
+
+class Square:
+    """ Creating a class named 'Square' with a private
+    instance attribute size. """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ initializing the size and position attributes
+        and checking if the size is an integer
+        and if it is greater than zero"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Retrives the value of size. """
+        return self.__size
+
+    @property
+    def position(self):
+        """Retrives the value of position. """
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size. """
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @position.setter
+    def position(self, value):
+        if not (isinstance(self.__position, tuple) and len(self.__position) == \
+                2 and all(isinstance(i, int) for i in self.__position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """ Returns the current square area. """
+        return self.__size**2
+
+    def my_print(self):
+        """ prints in stdout the square with the character # """
+        if self.__size == 0:
+            print()
+        else:
+            i = 0
+            while i < self.__size:
+                x = 0
+                while x < self.__position[0]:
+                    print(" ", end="")
+                    x += 1
+
+                j = 0
+                while j < self.__size:
+                    print("#", end="")
+                    j += 1
+
+                print()
+                i += 1
