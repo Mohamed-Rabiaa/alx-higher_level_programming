3a84a6c9817d2e0dc678cefa7c7621ee2c2dcc6c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-29 08:59:21 -0800

    Adding area & perimeter funcs to the Rectangle class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This module contains the Rectangle class"""
+""" This module contains the Rectangle class """
 
 
 class Rectangle:
@@ -14,12 +14,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """Returns the width of the rectangle"""
+        """ Returns the width of the rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the value of the width of the rectangle"""
+        """ Sets the value of the width of the rectangle """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -28,14 +28,24 @@ class Rectangle:
 
     @property
     def height(self):
-        """Returns the height of the rectangle"""
+        """ Returns the height of the rectangle """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the value of the width of the rectangle"""
+        """ Sets the value of the width of the rectangle """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """ Returns the rectangle area """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
