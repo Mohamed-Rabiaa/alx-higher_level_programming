d26a6e15ad09a32370e1e407252971437526f40a
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-05 10:00:40 -0800

    Adding the lookup func

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is the lookup module """
+
+
+def lookup(obj):
+    """ This function returns the list of available
+    attributes and methods of an object """
+
+    return dir(obj)
