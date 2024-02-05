9d1c6d6923e568dc8928ef8626bb53e9400c8bea
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-05 12:33:56 -0800

    Adding area func to the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module contains the BaseGeometry class """
+
+
+class BaseGeometry:
+    """ The BaseGeometry class defines one function, area() """
+    def area(self):
+        """ This function raises an Exception with the message area()
+        is not implemented """
+        raise Exception("area() is not implemented")
