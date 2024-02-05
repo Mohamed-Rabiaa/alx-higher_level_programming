436d079c54160df000a282ac4e361964195daabf
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-05 10:08:26 -0800

    Adding 1-my_list module

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This is the 1-my_list module """
+
+
+class MyList(list):
+    """ class MyList defines one function print_sorted """
+
+    def print_sorted(self):
+        """ This function prints the list, but sorted (ascending sort) """
+        print(sorted(self))
