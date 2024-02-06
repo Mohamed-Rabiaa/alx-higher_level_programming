a1425aea183c0e1788008de2904540a7f5b0afd2
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-06 12:23:07 -0800

    Fixing 5-save_to_json_file func

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,11 @@ from json import dumps
 def save_to_json_file(my_obj, filename):
     """ This function writes an Object to a text file,
     using a JSON representation """
-    s = dumps(my_obj)
+    s = ""
+    try:
+        s = dumps(my_obj)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(s)
