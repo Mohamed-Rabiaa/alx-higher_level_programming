ef1cd8912e5b93753a4d2d5cddccb04e77585a04
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-08 10:59:53 -0800

    Adding the Student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains the Student class """
+
+
+class Student:
+    """ This is the Student class """
+    def __init__(self, first_name, last_name, age):
+        """ This function instantiates first_name, last_name and age """ 
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ This function retrieves a dictionary representation of a Student instance """
+        return self.__dict__
