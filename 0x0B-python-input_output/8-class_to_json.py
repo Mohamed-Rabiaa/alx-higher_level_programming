52dee04c8f03cd9dd0449f926dd7aeb8783fc044
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-08 10:08:00 -0800

    Fixing class_to_json func

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """ This module contains the class_to_json function """
 
 
-import json
-
-
 def class_to_json(obj):
     """
     This function returns the dictionary description with simple data structure
