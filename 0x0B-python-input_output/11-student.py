ab38d78540fa82f34ce2a13aa04af4fa97027682
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-08 14:04:37 -0800

    Adding reload_from_json func

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module contains the Student class """
+
+
+class Student:
+    """ This is the Student class """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        This function instantiates first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This function retrieves a dictionary representation of a Student
+        instance
+        """
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            dct = {}
+            for item in attrs:
+                if item in self.__dict__.keys():
+                    dct[item] = self.__dict__[item]
+            return dct
+
+    def reload_from_json(self, json):
+        """
+        This function replaces all attributes of the Student instance
+        """
+        if json:
+            self.first_name = json['first_name']
+            self.last_name = json['last_name']
+            self.age = json['age']
