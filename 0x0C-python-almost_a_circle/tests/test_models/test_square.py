a0694e5a1a46cf201bf907b72699c8f5a6ea1482
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-12 16:23:08 -0800

    f

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -44,3 +44,62 @@ class TestSquare(unittest.TestCase):
     def test_square_size_setter(self):
         self.s1.size = 25
         self.assertEqual(self.s1.size, 25)
+
+        with self.assertRaises(TypeError):
+            self.s1.size = "2"
+
+        with self.assertRaises(ValueError):
+            self.s1.size = -5
+
+    def test_square_update(self):
+        self.s1 = Square(8, 8, 8, 8)
+
+        self.s1.update(11)
+        self.assertEqual(self.s1.id, 11)
+        self.assertEqual(self.s1.size, 8)
+        self.assertEqual(self.s1.x, 8)
+        self.assertEqual(self.s1.y, 8)
+
+        self.s1.update(11, 17)
+        self.assertEqual(self.s1.id, 11)
+        self.assertEqual(self.s1.size, 17)
+        self.assertEqual(self.s1.x, 8)
+        self.assertEqual(self.s1.y, 8)
+
+        self.s1.update(12, 18, 4)
+        self.assertEqual(self.s1.id, 12)
+        self.assertEqual(self.s1.size, 18)
+        self.assertEqual(self.s1.x, 4)
+        self.assertEqual(self.s1.y, 8)
+
+        self.s1.update(30, 8, 2, 3)
+        self.assertEqual(self.s1.id, 30)
+        self.assertEqual(self.s1.size, 8)
+        self.assertEqual(self.s1.x, 2)
+        self.assertEqual(self.s1.y, 3)
+
+        self.s1 = Square(7, 7, 7, 7)
+
+        self.s1.update(id=22)
+        self.assertEqual(self.s1.id, 22)
+        self.assertEqual(self.s1.size, 7)
+        self.assertEqual(self.s1.x, 7)
+        self.assertEqual(self.s1.y, 7)
+
+        self.s1.update(id=28, size=33)
+        self.assertEqual(self.s1.id, 28)
+        self.assertEqual(self.s1.size, 33)
+        self.assertEqual(self.s1.x, 7)
+        self.assertEqual(self.s1.y, 7)
+
+        self.s1.update(id=99, size=19, x=9)
+        self.assertEqual(self.s1.id, 99)
+        self.assertEqual(self.s1.size, 19)
+        self.assertEqual(self.s1.x, 9)
+        self.assertEqual(self.s1.y, 7)
+
+        self.s1.update(id=14, size=20, x=2, y=2)
+        self.assertEqual(self.s1.id, 14)
+        self.assertEqual(self.s1.size, 20)
+        self.assertEqual(self.s1.x, 2)
+        self.assertEqual(self.s1.y, 2)
