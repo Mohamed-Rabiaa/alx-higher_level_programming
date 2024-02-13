05a02fd61d6872a74f944829b5117dac182d6b77
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-02-12 17:27:14 -0800

    h

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,11 +1,14 @@
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
 
     def setUp(self):
-        Base._Base__nb_objects = 0
+        # Base._Base__nb_objects = 0
+        pass
 
     def test_id(self):
         b1 = Base()
@@ -22,3 +25,20 @@ class TestBase(unittest.TestCase):
 
         b5 = Base()
         self.assertEqual(b5.id, 4)
+
+    def test_to_json_string(self):
+        rect = Rectangle(10, 7, 2, 8, 22)
+        dictionary = rect.to_dictionary()
+        json_dct = '[{"x": 2, "y": 8, "id": 22, "height": 7, "width": 10}]'
+        self.assertEqual(Base.to_json_string([dictionary]), json_dct)
+
+        sq = Square(5, 3, 3, 88)
+        dictionary = sq.to_dictionary()
+        json_dct = '[{"id": 88, "x": 3, "size": 5, "y": 3}]'
+        self.assertEqual(Base.to_json_string([dictionary]), json_dct)
+
+        sq = Square(15, 4, 4, 66)
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+        sq = Square(15, 4, 4, 66)
+        self.assertEqual(Base.to_json_string(None), "[]")
